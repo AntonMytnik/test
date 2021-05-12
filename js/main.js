e7f08ef057eeb4d0726dd8ebb654baabd51225f8
Anton Mytnik <gamvip1069@gmail.com>
2021-05-12 11:57:55 +0200

    83

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-// console.log('Hello World');
+$(document).ready(() => {
+   console.log('Hello World');
+   $('nav').click(() => {
+      console.log(writelorem());
+   });
+   function writelorem() {
+      return ('Lorem ipsum dolor sit amet, consectetur adipiscing elit');
+   }
+});
